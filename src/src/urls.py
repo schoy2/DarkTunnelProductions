4cabb01a3f0782f27044f62af8cfc6071ecd671e
schoy2 <samuelchoy2@gmail.com>
2017-08-05 09:54:20 -0600

    First Commit
    This is the first commit

--- src/src/urls.py
+++ src/src/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
-
+#//This is a test 
 from menu.views import (
 	#menu_listview,
 	MenuListView,
